dbfb48b05999a05085f599f6b418a83319c60961
harshagrawal-coder <harshagrawal9498@gmail.com>
2026-03-12 14:51:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from "@/Component/Section/Projects";
 import Contact from "@/Component/Section/Contact";
 import Testimonial from "@/Component/Section/Testimonial";
 import Experience from "@/Component/Section/Experience";
-import Navbar from "./Component/Layout/Navbar";
+import Navbar from "./Component/Layout/Navbar"; 
 const App = () => {
   return (
     <>
